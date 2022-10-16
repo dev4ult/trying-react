d02f8307a9bfe3b1ede6f4e29c3a13819df504e2
dev4ult <nibrasbiasaaja@gmail.com>
2022-10-16 21:53:20 +0700

    trying useEffect

--- public/app.js
+++ public/app.js
@@ -1,11 +1,19 @@
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-function alertMe() {
-  alert('hello World!');
+function App() {
+  const [count, setCount] = React.useState(0);
+  const [click, setClick] = React.useState(false);
+
+  // trying useEffect
+  React.useEffect(function () {
+    console.log(document.querySelector('.newDiv'));
+  }, [click]);
+  return /*#__PURE__*/React.createElement("div", {
+    className: "newDiv"
+  }, /*#__PURE__*/React.createElement("button", {
+    onClick: setClick.bind(this, true)
+  }, "Click me"), /*#__PURE__*/React.createElement("button", {
+    onClick: setCount.bind(this, count + 1)
+  }, "Tambah"), /*#__PURE__*/React.createElement("span", null, "Count saat ini ", count));
 }
-const world = 'earth';
-const element = /*#__PURE__*/React.createElement("div", {
-  className: "box"
-}, /*#__PURE__*/React.createElement("button", {
-  onClick: alertMe
-}, "Click me!"));
+const element = /*#__PURE__*/React.createElement("main", null, /*#__PURE__*/React.createElement("h2", null, "Something here"), /*#__PURE__*/React.createElement(App, null));
 root.render(element);
--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-function alertMe() {
-  alert('hello World!');
+function App() {
+  const [count, setCount] = React.useState(0);
+  const [click, setClick] = React.useState(false);
+
+  // trying useEffect
+  React.useEffect(
+    function () {
+      console.log(document.querySelector('.newDiv'));
+    },
+    [click]
+  );
+
+  return (
+    <div className="newDiv">
+      <button onClick={setClick.bind(this, true)}>Click me</button>
+      <button onClick={setCount.bind(this, count + 1)}>Tambah</button>
+      <span>Count saat ini {count}</span>
+    </div>
+  );
 }
 
-const world = 'earth';
 const element = (
-  <div className="box">
-    <button onClick={alertMe}>Click me!</button>
-  </div>
+  <main>
+    <h2>Something here</h2>
+    <App />
+  </main>
 );
 
 root.render(element);
