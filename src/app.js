829b71cba5a94c3b0f87cc49033dcf71b7b61732
dev4ult <nibrasbiasaaja@gmail.com>
2022-10-27 22:52:07 +0700

    added boxes challenge

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import Box from './components/Box';
+import boxes from './boxes';
+// import Header from './Header';
+// import Main from './Main';
+// import Footer from './Footer';
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
+  const [squares, setSquares] = React.useState(boxes);
+
+  function toggle(id) {
+    setSquares((prevSquares) => {
+      return prevSquares.map((square) => {
+        return square.id === id ? { ...square, on: !square.on } : square;
+      });
+    });
+  }
+
+  const boxesElement = squares.map((square) => {
+    return <Box key={square.id} handleClick={toggle.bind(this, square.id)} selectedBox={square} />;
+  });
+
+  return <div className="container">{boxesElement}</div>;
 }
---
+++ src/components/Box.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Box(props) {
+  return <div className={`box ${props.selectedBox.on ? 'on' : 'off'}`} onClick={props.handleClick}></div>;
+}
