1c1b8209a28c6496ff4df12c70129133dc6b5bfa
dev4ult <nibrasbiasaaja@gmail.com>
2022-10-31 20:59:25 +0700

    controlled input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 export default function Form() {
+  const [formData, setFormData] = React.useState({
+    firstName: '',
+    middleName: '',
+    lastName: '',
+    comment: '',
+  });
+
+  function handleChange(e) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
+  const { firstName, middleName, lastName, comment } = formData;
+
   return (
-    <form>
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Middle Name" />
-      <input type="text" placeholder="Last Name" />
+    <form className="flex">
+      <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} value={firstName} />
+      <input type="text" name="middleName" placeholder="Middle Name" onChange={handleChange} value={middleName} />
+      <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} value={lastName} />
+      <textarea placeholder="comments" name="comment" onChange={handleChange} value={comment}></textarea>
     </form>
   );
 }
