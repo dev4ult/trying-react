719b20d73620d50502a98e47dcc6ddfd6d336fed
dev4ult <nibrasbiasaaja@gmail.com>
2022-11-02 09:15:49 +0700

    know useEffect

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Signup from './components/Signup';
 import './css/styles.css';
 
 export default function App() {
+  const [starWarsData, setStarWarsData] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('https://swapi.dev/api/people/1')
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data));
+  }, []);
+
   return (
     <div className="App">
-      <Signup />
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
 }
